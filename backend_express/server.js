ca8f137061ff1e738b0abbb0cbe6bd39dff023a7
Rgforevr-Smithy <prkuruvi@cougarnet.uh.edu>
2022-03-13 23:27:44 -0500

    Update server.js

--- backend_express/server.js
+++ backend_express/server.js
@@ -122,4 +122,4 @@ app.post("/pricingmodule", (req, res) => { //retrieve
     
 })
 
-app.listen(port, () => {console.log("Server started on port 5000")});
+app.listen(port, () => {console.log(`Server started on port ${port}`)});
